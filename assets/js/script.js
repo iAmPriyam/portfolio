6ab2c6997b0265f0e03f714608faad306cbb5e04
Aditya.Priyam <aditya.priyam@ril.com>
2021-03-06 01:51:28 +0530

    update to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
-$(document).ready(function(){
-    $('.hero-image-wrapper').tilt({
-        maxTilt : 10
-    });
-});
+$(document).ready(function () {
+  $('.hero-image-wrapper').tilt({
+    maxTilt: 10
+  })
+})
 
-window.onscroll= function(){
-    showButton();
+window.onscroll = function () {
+  showButton()
 }
 
+const btn = document.getElementById('scroll-btn')
 
-const btn= document.getElementById("scroll-btn");
-
-function showButton(){
-    if(document.body.scrollTop>1000 || document.documentElement.scrollTop >1000){
-        btn.style.display="block";
-    }
-    else{
-        btn.style.display="none";
-    }
+function showButton() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    btn.style.display = 'block'
+  } else {
+    btn.style.display = 'none'
+  }
 }
 
-function scrollToTop(){
-    document.body.scrollTop=0;
-    document.documentElement.scrollTop=0;
+function scrollToTop() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 }
-            
+
+// const collapsableHeader = document.querySelectorAll('.exp-header')
+
+// const toggleBoxState = (event) => {
+//   console.log(event.target.className)
+// }
+
+// collapsableHeader.forEach((item) =>
+//   item.addEventListener('click', toggleBoxState)
+// )
